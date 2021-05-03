2e28092588c73d0275ff34949c9060a89068ab30
Dmitry Ermakov <dmitr.ermakov1@gmail.com>
2021-05-03 22:13:33 +0300

    dev

--- ParseTargets.py
+++ ParseTargets.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 
-import numpy as np
 import os
 import csv
 import subprocess
+import numpy as np
 
 
-#print(os.environ["RLS1_x"])
+def create_struct_id_with_coord(filename):
+    targets = {}
+    with open(filename, encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=",")
+        for row in file_reader:
+            target_id = row[0]
+            target_xy = np.array([int(row[1][1:]), int(row[2][1:])])
+            # тут надо выделить дробную часть из координат
+            targets[target_id] = target_xy
+    return targets
+
+
+targets_old = create_struct_id_with_coord('Files/TargetsDataOldStep.csv')
+targets_cur = create_struct_id_with_coord('Files/TargetsDataCurStep.csv')
+
+for key, value in targets_cur.items():
+    print(targets_old.get(key))
+
+# print(os.environ["RLS1_x"])
--- main.sh
+++ main.sh
@@ -2,22 +2,42 @@
 
 source ./InitParameters.sh
 
-filename=LastTargets.csv
+mkdir -p Files
+filename=`pwd`/Files/LastTargets.csv
 last_targets_id=$filename
 :> $last_targets_id
-num_targets=10  # number of latest targets
+num_targets=30  # number of latest targets
 targets_dir=/tmp/GenTargets/Targets/
+
+# first step (gen initial condition)
+OldStepDir=`pwd`/Files/TargetsDataOldStep.csv
+CurStepDir=`pwd`/Files/TargetsDataCurStep.csv
+
+ls -t /tmp/GenTargets/Targets/ | sort | tail -n $num_targets > $last_targets_id  # find last n files
+cat /dev/null > $OldStepDir
+while read line
+    do
+      echo -n ${line:12}',' >> $OldStepDir
+      cat $targets_dir$line >> $OldStepDir
+  done < $filename
+
 for ((;;))
 do
-  ls -t /tmp/GenTargets/Targets/ | tail -n $num_targets >> $last_targets_id
-  sleep 5
-  # тут будет что-то происходить
+  sleep 1  # delta_t >= Sleeptime
+
+  ls -t /tmp/GenTargets/Targets/ | sort | tail -n $num_targets > $last_targets_id  # find last n files
+  cat /dev/null > $CurStepDir
+
   while read line
     do
-      echo ${line:12}; cat $targets_dir$line
+      echo -n ${line:12}',' >> $CurStepDir
+      cat $targets_dir$line >> $CurStepDir
   done < $filename
-  # до сюда
-  cat /dev/null > $filename
+
+  # calc
+  python3 ParseTargets.py
+
+  cat $CurStepDir > $OldStepDir  # rewrite (x_k -> x_{k-1})
   echo iter
 done
 
