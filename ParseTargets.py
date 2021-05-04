f476559cf3ee667c4454f01c74d1d6510943110c
Dmitry Ermakov <dmitr.ermakov1@gmail.com>
2021-05-04 12:47:28 +0300

    until remove old step

--- ParseTargets.py
+++ ParseTargets.py
@@ -1,27 +1,78 @@
 #!/usr/bin/env python
 
-import os
+# import os
 import csv
-import subprocess
+# import subprocess
 import numpy as np
+import math
 
 
-def create_struct_id_with_coord(filename):
+def createStructIdWithCoord(filename):
     targets = {}
     with open(filename, encoding='utf-8') as r_file:
         file_reader = csv.reader(r_file, delimiter=",")
         for row in file_reader:
             target_id = row[0]
-            target_xy = np.array([int(row[1][1:]), int(row[2][1:])])
-            # тут надо выделить дробную часть из координат
+            target_xy = np.array([float(row[1][1:]) / 1000, float(row[2][1:]) / 1000])
             targets[target_id] = target_xy
     return targets
 
 
-targets_old = create_struct_id_with_coord('Files/TargetsDataOldStep.csv')
-targets_cur = create_struct_id_with_coord('Files/TargetsDataCurStep.csv')
+def checkPoint(radius, x, y, percent, startAngle):
+    # calculate endAngle
+    endAngle = 360 / percent + startAngle
 
-for key, value in targets_cur.items():
-    print(targets_old.get(key))
+    # Calculate polar co-ordinates
+    polarradius = math.sqrt(x * x + y * y)
+    Angle = math.atan(y / x)
+
+    # Check whether polarradius is less
+    # then radius of circle or not and
+    # Angle is between startAngle and
+    # endAngle or not
+    if startAngle <= Angle <= endAngle and polarradius < radius:
+        print("Point (", x, ",", y, ") "
+                                    "exist in the circle sector")
+        return True
+    else:
+        print("Point (", x, ",", y, ") "
+                                    "does not exist in the circle sector")
+        return False
+
+
+def checkCollision(a, b, c, x, y, radius):
+    # Finding the distance of line
+    # from center.
+    dist = ((abs(a * x + b * y + c)) /
+            math.sqrt(a * a + b * b))
+
+    # Checking if the distance is less
+    # than, greater than or equal to radius.
+    if (radius == dist):
+        print("Touch")
+    elif (radius > dist):
+        print("Intersect")
+    else:
+        print("Outside")
+
+
+def checkRLS(point, RLS_x, RLS_y, RLS_alpha, RLS_range, RLS_angle):
+    current_x = point[0] - RLS_x
+    current_y = point[1] - RLS_y
+    percent = RLS_angle / 360
+    start_angle = RLS_alpha - RLS_range / 2
+    return checkPoint(RLS_range, current_x, current_y, percent, start_angle)
+
+
+targets_old = createStructIdWithCoord('Files/TargetsDataOldStep.csv')
+targets_cur = createStructIdWithCoord('Files/TargetsDataCurStep.csv')
+
+for key, value in targets_old.items():
+    cur_xy = targets_cur.get(key)
+    #print(value, '-', cur_xy)
+
+    # check RLS1
+
+# need black list points
 
 # print(os.environ["RLS1_x"])
--- main.sh
+++ main.sh
@@ -3,42 +3,40 @@
 source ./InitParameters.sh
 
 mkdir -p Files
-filename=`pwd`/Files/LastTargets.csv
-last_targets_id=$filename
-:> $last_targets_id
-num_targets=30  # number of latest targets
+last_targets=`pwd`/Files/LastTargets.csv
 targets_dir=/tmp/GenTargets/Targets/
-
-# first step (gen initial condition)
 OldStepDir=`pwd`/Files/TargetsDataOldStep.csv
 CurStepDir=`pwd`/Files/TargetsDataCurStep.csv
+:> $last_targets
+num_targets=5  # number of last targets
 
-ls -t /tmp/GenTargets/Targets/ | sort | tail -n $num_targets > $last_targets_id  # find last n files
+# first step (gen initial condition)
+ls -t $targets_dir | sort | tail -n $num_targets > $last_targets  # find last n files
 cat /dev/null > $OldStepDir
 while read line
     do
       echo -n ${line:12}',' >> $OldStepDir
       cat $targets_dir$line >> $OldStepDir
-  done < $filename
+  done < $last_targets
 
+delta_t=10
 for ((;;))
 do
-  sleep 1  # delta_t >= Sleeptime
 
-  ls -t /tmp/GenTargets/Targets/ | sort | tail -n $num_targets > $last_targets_id  # find last n files
+  sleep $delta_t  # delta_t >= Sleeptime
+  ls -t $targets_dir | sort | tail -n $num_targets > $last_targets  # find last n files
   cat /dev/null > $CurStepDir
 
   while read line
     do
       echo -n ${line:12}',' >> $CurStepDir
       cat $targets_dir$line >> $CurStepDir
-  done < $filename
+  done < $last_targets
 
   # calc
   python3 ParseTargets.py
-
   cat $CurStepDir > $OldStepDir  # rewrite (x_k -> x_{k-1})
-  echo iter
+  # echo iter
 done
 
 #python3 ParseTargets.py
